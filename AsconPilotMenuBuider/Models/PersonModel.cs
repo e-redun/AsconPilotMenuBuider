8806c9b10e4883e57a617c8bac26c3360af3f15d
e-redun <e.redun@mail.ru>
2024-07-03 20:48:09 +0500

    CommonLib, структура

---
+++ AsconPilotMenuBuider/Models/PersonModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsconPilotMenuBuider.Models
+{
+    public class PersonModel
+    {
+        private string _name;
+        private int age;
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public int Age
+        { 
+            get => age;
+            set => age = value; 
+        }
+    }
+}
